e8692f88fedbfa39d278b4b9965c2cf06b420bec
borisminardo <borisminardo1@gmail.com>
2024-02-26 15:17:28 +0100

    router code improved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Redirect,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { About, Experience, Projects, Contacts } from "./componets";
 import { Header, Body, Footer } from "./containers";
-import { Outlet, useLocation, useNavigate, useRoutes } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "react-awesome-slider/dist/styles.css";
@@ -31,14 +25,12 @@ const App = () => {
       <div className="App">
         <Header />
         <Body>
-          <Router>
-            <Routes>
-              <Route path="/about" component={About} />
-              <Route path="/experience" component={Experience} />
-              <Route path="/projects" component={Projects} />
-              <Route path="/contacts" component={Contacts} />
-            </Routes>
-          </Router>
+          <Routes>
+            <Route path="/about" component={About} />
+            <Route path="/experience" component={Experience} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contacts" component={Contacts} />
+          </Routes>
         </Body>
         <Footer />
       </div>
